5a2eeedfa8515e8413a2df3fb9cad36ecd781bd9
soumya-ranjan-000 <rakiraja7751841655@gmail.com>
2024-01-07 14:11:19 +0530

    added few test sceanrios as part of Login functionality

--- src/test/java/com/soumya/qa/base/Utilities.java
+++ src/test/java/com/soumya/qa/base/Utilities.java
@@ -1,6 +1,6 @@
 package com.soumya.qa.base;
 
 public class Utilities {
-    public static final long IMPLICIT_WAIT_TIME = 4000L;
-    public static final long PAGE_LOAD_TIME = 6000L;
+    public static final long IMPLICIT_WAIT_TIME = 10;
+    public static final long PAGE_LOAD_TIME = 5;
 }
--- src/test/java/com/soumya/qa/testcases/Login.java
+++ src/test/java/com/soumya/qa/testcases/Login.java
@@ -1,41 +1,73 @@
 package com.soumya.qa.testcases;
 
 import com.soumya.qa.base.Base;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
+import java.util.Date;
+
+@Test
 public class Login extends Base {
-
     WebDriver driver;
 
-    @Test
-    public  void verifyLoginWithValidCredentialsWithChrome() throws InterruptedException {
-        driver = initializeBrowser("chrome");
-        Thread.sleep(3000L);
-        driver.get("http://tutorialsninja.com/demo/");
-        Thread.sleep(3000L);
+    @BeforeMethod
+    public void setUp(){
+        driver = initializeBrowserAndOpenApplicationURL("chrome");
     }
 
     @Test
-    public  void verifyLoginWithValidCredentialsWithFireFox() throws InterruptedException {
-        driver = initializeBrowser("firefox");
-        Thread.sleep(3000L);
-        driver.get("http://tutorialsninja.com/demo/");
-        Thread.sleep(3000L);
+    public void verifyLoginWithValidCredentials(){
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("input-email")).sendKeys("rakiraja7751841655@gmail.com");
+        driver.findElement(By.id("input-password")).sendKeys("soumya@123");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+        Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
     }
 
     @Test
-    public  void verifyLoginWithValidCredentialsWithEdge() throws InterruptedException {
-        driver = initializeBrowser("edge");
-        Thread.sleep(3000L);
-        driver.get("http://tutorialsninja.com/demo/");
-        Thread.sleep(3000L);
+    public void verifyLoginWithInvalidCredentials(){
+        long currentTime = new Date().getTime();
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("input-email")).sendKeys("xyzabc"+currentTime+"@gmail.com");
+        driver.findElement(By.id("input-password")).sendKeys("xyzabc123");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Warning: No match for E-Mail Address and/or Password.']")).
+                isDisplayed(),"Expected Warning msg not displayed" );
     }
 
-    @AfterTest
+    @Test
+    public void verifyLoginWithInvalidEmailAndPassword(){
+        long currentTime = new Date().getTime();
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("input-email")).sendKeys("xyzabc"+currentTime+"@gmail.com");
+        driver.findElement(By.id("input-password")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Warning: No match for E-Mail Address and/or Password.']")).
+                isDisplayed(),"Expected Warning msg not displayed" );
+    }
+
+    @Test
+    public void verifyLoginWithValidEmailIdAndInvalidPassword(){
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("input-email")).sendKeys("amotoori3@gmail.com");
+        driver.findElement(By.id("input-password")).sendKeys("xyzabc123");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Warning: No match for E-Mail Address and/or Password.']")).
+                isDisplayed(),"Expected Warning msg not displayed" );
+    }
+
+    @AfterMethod
     void tearDown(){
         quitDriver();
     }
-
 }
